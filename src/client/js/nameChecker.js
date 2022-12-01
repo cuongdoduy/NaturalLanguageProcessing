d1786efb0f5d7af8ad464d04a325c27e5371fe64
CuongDoDuy <cuongthtb08022004@gmail.com>
2022-12-02 03:26:49 +0700

    first commit

---
+++ src/client/js/nameChecker.js
@@ -0,0 +1,16 @@
+function isvalidURL(url){
+    var urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
+    '(\\#[-a-z\\d_]*)?$','i'); // validate fragment locator
+  return !!urlPattern.test(url);
+}
+export function checkForName(inputText) {
+    if (inputText=="")  
+    {window.alert("Srroy, you have to fill in the blank"); return false;}
+    else
+    if(isvalidURL(inputText)==false) {window.alert("Wrong URL, please check your URL again");    return false;}
+    return true;
+}
